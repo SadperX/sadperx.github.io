9df26549a2c32c8cfdb7b58e12d3d082adc1f88b
Sadper <sadper32@gmail.com>
2024-04-08 23:51:29 -0600

    Basic Home Page

---
+++ index.js
@@ -0,0 +1,30 @@
+var yt_button = document.getElementById('YT');
+
+yt_button.addEventListener("click", function() {
+   window.location.href = "https://www.youtube.com/";
+});
+
+var gh_button = document.getElementById('GH');
+
+gh_button.addEventListener("click", function() {
+   window.location.href = "https://www.github.com/";
+});
+
+var cr_button = document.getElementById('CR');
+
+cr_button.addEventListener("click", function() {
+   window.location.href = "https://htmlcolors.com/";
+});
+
+var tw_button = document.getElementById('TW');
+
+tw_button.addEventListener("click", function() {
+   window.location.href = "https://www.twitch.tv/";
+});
+
+var dv_button = document.getElementById('DV');
+
+dv_button.addEventListener("click", function() {
+   window.location.href = "https://www.devdocs.io/";
+});
+
